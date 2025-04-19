7ecf15bcedaa65ce3d6c05de308b977b4d710016
Kalaganova-Tyo <tiwared@yandex.ru>
2025-04-19 20:51:41 +0300

    Add scraper.py and model.py

---
+++ model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    price = Column(String)
+    datetime = Column(DateTime)
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///cars.db')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    print("База данных cars и таблица 'cars' успешно созданы.")
